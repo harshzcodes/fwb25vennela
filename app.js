ef15b7b6fc0a7d4f7f57b24127c3f6986235618e
Vennela <S544925@nwmissouri.edu>
2021-11-03 12:36:08 -0500

    added selector.pug to repo

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jewelryRouter = require('./routes/jewelry');
-var addmodsRouter = require('./routes/addmods')
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jewelry',jewelryRouter);
 app.use('/addmods',addmodsRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
